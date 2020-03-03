31375a1adc6a5a831ccf99d5b1fb9b1d59b355d3
mob-sakai <sakai861104@gmail.com>
2020-03-03 18:45:10 +0900

    refactor: refactor

--- Packages/UIParticle/Scripts/UIParticle.cs
+++ Packages/UIParticle/Scripts/UIParticle.cs
@@ -381,11 +381,9 @@ namespace Coffee.UIExtensions
                     }
                     Profiler.EndSample();
 
-                    // Editor crashes when mesh is set to null when ParticleSystem.RenderMode=Mesh
+                    // #69: Editor crashes when mesh is set to null when ParticleSystem.RenderMode=Mesh
                     if (_renderer.renderMode == ParticleSystemRenderMode.Mesh && !_renderer.mesh)
-                    {
                         return;
-                    }
 
                     // #61: When ParticleSystem.RenderMode=None, an error occurs
                     if (_renderer.renderMode == ParticleSystemRenderMode.None)

1775713c2dbeef09ad3eb1f49b53cf44bf61d535
mob-sakai <sakai861104@gmail.com>
2020-09-28 18:22:04 +0900

    fix: do not bake particle system to mesh when the alpha is zero
    close #102

--- Packages/UIParticle/Scripts/UIParticleUpdater.cs
+++ Packages/UIParticle/Scripts/UIParticleUpdater.cs
@@ -196,6 +196,10 @@ namespace Coffee.UIExtensions
                     Profiler.EndSample();
                 }
 
+                // #102: Do not bake particle system to mesh when the alpha is zero.
+                if (Mathf.Approximately( particle.canvasRenderer.GetInheritedAlpha(), 0))
+                    continue;
+
                 // Bake main particles.
                 var r = currentPs.GetComponent<ParticleSystemRenderer>();
                 if (CanBakeMesh(r))
